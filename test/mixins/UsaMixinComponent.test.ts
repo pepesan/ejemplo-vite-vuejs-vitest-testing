e0910b6c06f74d0781c261caaca44849f937ffc2
pepesan <pepesan@gmail.com>
2024-12-05 13:18:24 +0100

    mejorado ejemplo de uso de mixin y de pruebas asociadas

--- test/mixins/UsaMixinComponent.test.ts
+++ test/mixins/UsaMixinComponent.test.ts
@@ -1,14 +1,14 @@
-import { describe, it, expect } from 'vitest';
+import {describe, expect, test} from 'vitest';
 import { mount } from '@vue/test-utils';
 import UsaMixinComponent from '../../src/components/mixins/UsaMixinComponent.vue';
 
 describe('MyComponent', () => {
-    it('debería renderizar el mensaje desde el mixin', () => {
+    test('debería renderizar el mensaje desde el mixin', () => {
         const wrapper = mount(UsaMixinComponent);
         expect(wrapper.text()).toContain('Hola desde el mixin');
     });
 
-    it('debería llamar al método saludar al hacer clic en el botón', async () => {
+    test('debería llamar al método saludar al hacer clic en el botón', async () => {
         const mockConsole = vi.spyOn(console, 'log').mockImplementation(() => {});
         const wrapper = mount(UsaMixinComponent);
 
